25109453652885da2e32df54503166d8af76b5f7
AnaliceMarques <analicemarquessl@gmail.com>
2023-03-24 20:24:22 -0300

    Card style

---
+++ marvel-comics/src/context/GlobalContext.js
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useState } from "react";
+import { api } from "../service/api";
+
+export const GlobalContext = createContext();
+
+export function GlobalContextProvider({ children }) {
+  const [comicsList, setComicsList] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getComics = async () => {
+    try {
+      const res = await api.get("/comics");
+      console.log(res.data.data.results);
+      setComicsList(res.data.data.results);
+      setIsLoaded(true);
+    } catch (err) {
+      setError(true);
+      console.log(err.response);
+    }
+  };
+
+  useEffect(() => {
+    getComics();
+  }, []);
+
+  return (
+    <GlobalContext.Provider value={{ comicsList, isLoaded, error }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
